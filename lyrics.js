3942c49603e2ed1afad74cc0c420744632f50009
kalentara <kalentara@gmail.com>
2018-05-11 14:50:29 -0700

    lyric methodology

--- lyrics.js
+++ lyrics.js
@@ -1,16 +1,50 @@
 const Lyricist = require('lyricist/node6');
 const lyricist = new Lyricist('wj4t6ZnMsotFYe9tCuXQT2JIhAi9QeNmkKDFUplMNoZBJRyZfRAWAYer9TBP3XPR');
+const fs = require('fs');
 
-//var Genius = require("node-genius");
-//var geniusClient = new Genius('wj4t6ZnMsotFYe9tCuXQT2JIhAi9QeNmkKDFUplMNoZBJRyZfRAWAYer9TBP3XPR');
 
-//var songID = results.id;
+// search song
+// check guy who made song ( artist ID)
+// see if artistID matches the artist that you search
+// (else check next)
+// if the artists match (song is same) : return the lyrics 
+// send the lyrics to app
 
-lyricist.search("lift yourself").then((song) => {
-	//console.log(song);
-  return song[0].id;
-}).then((songId) => {
-  lyricist.song(songId, {fetchLyrics: true}).then((song) => {
-    console.log(song.lyrics);
-  });
+//note: make this into 3 parts. 1 finds artist ID. 2 finds the artist ID on the song. 3 selects the lyric from the info
+var selectLyric = (enterSong, tagArtist) => {
+	lyricist.search(enterSong).then((song) => {
+
+		var numVal = 0;
+		var artboy = lyricist.artist(song[numVal].id);
+
+		while (tagArtist != artboy.name){
+			numVal += 1; 		
+		}; 
+		return song[numVal].id;
+
+	}).then((songId) => {
+  		lyricist.song(songId, {fetchLyrics: true}).then((song) => {
+    		//fs.writeFileSync('lyric.json', JSON.stringify(song.lyrics));
+  		});
+	});
+};
+
+selectLyric('lift yourself', 'kanye west');
+
+
+// this is app command 
+// idea is that the webpage pulls this info from text on the page
+// runs this command to check if the song/artist are matching
+// then shoots the lyrics over to hbs file
+
+/**
+
+app.get('/', (request, response) => {
+	todo.selectLyric(song, artist);
+	response.render('lyric.hbs',{
+		title:'Song Lyrics',
+		lyrics: song.lyrics
+	})
 });
+
+*/
--- todo.js
+++ todo.js
@@ -1,10 +1,9 @@
 const fs = require('fs');
 const request = require('request');
-
-
-/**
- * This function loads the 'accounts.json' file
- */
+//kalen
+const Lyricist = require('lyricist/node6');
+const lyricist = new Lyricist('wj4t6ZnMsotFYe9tCuXQT2JIhAi9QeNmkKDFUplMNoZBJRyZfRAWAYer9TBP3XPR');
+//not kalen
 var loadFile = () => {
 	try {
 		return JSON.parse(fs.readFileSync('accounts.json'));
@@ -17,26 +16,18 @@ var loadFile = () => {
 	}
 };
 
-/**
- * This function checks if the account signing up is already contained in the file
- * @param {array} usersArr - List of users in the 'accounts.json' file
- * @param {string} username - The username that gets checked for duplicates
- */
+
 var duplicateUsers = (usersArr, username) => {
 	usersArr = loadFile();
-	if (username in usersArr) {
-		return 0
-	}else {
-		return 1
+	for (var i = 0; i < usersArr.length; i++) {
+		if(username == usersArr[i].user) {
+			return 0
+		}else {
+			return 1
+		}
 	}
 };
 
-/**
- * This function checks if the supplied account and password match
- * @param {array} usersArr - List of users in the 'accounts.json' file
- * @param {string} username - List of users in the 'accounts.json' file
- * @param {string} password - The username that gets checked for duplicates
- */
 var loginCheck = (usersArr, username, password) => {
 	usersArr = loadFile();
 	if(username in usersArr) {
@@ -52,12 +43,6 @@ var loginCheck = (usersArr, username, password) => {
 	}
 }
 
-
-/**
- * This function makes sure that the user has entered the password
- * @param {string} pass1 - The first password entered in the form
- * @param {string} pass2 - Supposed to be the same as the first pass
- */
 var passCheck = (pass1, pass2) => {
 	if(pass1 == pass2) {
 		return 1
@@ -66,42 +51,27 @@ var passCheck = (pass1, pass2) => {
 	}
 };
 
-
-/**
- * This function just creates the 'accounts.json' file
- * @param {array} usersArr - The list of accounts that will be written to the json
- */
 var writeFile = (usersArr) => {
 	fs.writeFileSync('accounts.json', JSON.stringify(usersArr));
 };
 
-/**
- * This function adds a user to the list
- * @param {array} userArr - The first password entered in the form
- * @param {string} pass2 - Supposed to be the same as the first pass
- */
-var addUser = (usersArr, username, password, name, question, answer) => {
+
+var addUser = (usersArr, username, password) => {
 	usersArr = loadFile();
+	// var account = {
+	// 	user: username,
+	// 	pass: password
+	// };
+
+	// usersArr.push(account);
 	usersArr[username] = {
-		name: name,
 		pass: password,
 		playlist: [],
-		question: question,
-		answer: answer,
 		loggedin: 'no'
 	}
 	writeFile(usersArr);
 };
 
-
-/**
- * This function connects to the ticket master API to find the name of the concert and url dates
- * @async
- * @param {string} keyword - The artist that is featured or a part of the concert that is being searched for
- * @param {string} key - API key
- * @todo  this is being replaced by a new API in a future update
- * @requires request
- */
 var getConcert = (keyword, key) => {
 	return new Promise ((resolve, reject) => {
 		request({
@@ -119,14 +89,6 @@ var getConcert = (keyword, key) => {
 	});
 }
 
-
-/**
- * This function connects to the lastfm API to get tracks based on the search term
- * @async
- * @param {string} trackName - Name of the track that you want to search
- * @param {string} key - API key
- * @requires request
- */
 var getTracks = (trackName, key) => {
   return new Promise((resolve,reject) => {
     request({
@@ -138,7 +100,7 @@ var getTracks = (trackName, key) => {
       	console.log(error);
       }else if (body.results['opensearch:totalResults'] == 0) {
       	resolve({
-      		Error: 'Could not find song'
+      		'Could not find song': 'Could not find song'
       	});
       }else {
       	var trackObject = {};
@@ -154,11 +116,6 @@ var getTracks = (trackName, key) => {
   });
 };
 
-
-/**
- * This function changes the loggedin value to "no" when you log out.
- * @param {array} usersArr - The array that contains all the registered users
- */
 var logoutCheck = (usersArr) => {
 	usersArr = loadFile();
 	for (var user in Object.keys(usersArr)) {
@@ -169,33 +126,88 @@ var logoutCheck = (usersArr) => {
 	writeFile(usersArr);
 }
 
-/**
- * This function adds songs into a playlist contained in each users' property
- * @param {string} usersArr - The array that contains all the registered users
- * @param {string} song - Name of the song that will be added to your playlist
- */
-var addPlaylist = (usersArr, song, artist, image) => {
-	var checker = 1
+// var loginCheck = (usersArr, username, password) => {
+// 	var check = 0;
+// 	usersArr = loadFile();
+// 	for (var i = 0; i < usersArr.length; i++) {
+// 		if(username == usersArr[i].user && password == usersArr[i].pass) {
+// 			check = 1
+// 			break;
+// 		}
+// 	}
+// 	return check
+// };
+
+// var logoutCheck = (usersArr, username) => {
+// 	usersArr = loadFile();
+// 	for (var user in Object.keys(usersArr)) {
+// 		if(Object.keys(usersArr)[user] == username) {
+// 			if(Object.values(usersArr)[user].loggedin == "yes") {
+// 				console.log(Object.values(usersArr)[user].loggedin)
+// 				Object.values(usersArr)[user].loggedin = "no";
+// 				console.log(Object.values(usersArr)[user].loggedin)
+// 			}
+// 		}
+// 	}
+// 	writeFile(usersArr);
+// }
+
+var addPlaylist = (usersArr, song) => {
 	usersArr = loadFile();
 	for (var user in Object.keys(usersArr)) {
 		if(Object.values(usersArr)[user].loggedin == "yes") {
-			if (Object.values(usersArr)[user].playlist.includes(song)) {
-				checker = 0
+			if (song in Object.values(usersArr)[user].playlist) {
+				console.log('Song already existed')
 			}else {
-				var newObj = {song: {"artist":artist, "image":image}}
-				Object.values(usersArr)[user].playlist.push(newObj);
+				Object.values(usersArr)[user].playlist.push(song);
 			}
 		}
 	}
 	writeFile(usersArr);
-	return checker
 }
 
-/**
- * We can use these functions in another file now.
- * @type {{loadFile: loadFile, writeFile: writeFile, addUser: addUser, passCheck: passCheck, duplicateUsers: duplicateUsers, loginCheck: loginCheck, getTracks: (function(string, string): Promise<any>), logoutCheck: logoutCheck, addPlaylist: addPlaylist}}
- * @module exporting all the functions
- */
+// var showPlaylist = ();
+
 module.exports = {
 	loadFile, writeFile, addUser, passCheck, duplicateUsers, loginCheck, getTracks, logoutCheck, addPlaylist
 };
+
+// KALEN stuff
+var querySong = function(songName, artistName) {
+  return new Promise(function(resolve, reject) {
+    lyricist.search(songName).then((results) => { // results is the array of the returned search results
+      results.some((song, index, _arr) => { // Loop through the idvidual songs
+        if(artistName == "") { // if the aristName wasn't provided in searchForSong
+          console.log("No artist provided. Choosing random top song");
+          var randIdx = getRndInteger(0, results.length);
+          console.log("Index %d chosen", randIdx);
+          resolve(results[randIdx]);
+          return true;
+        }
+        else if (song.primary_artist.name.toLowerCase() == artistName.toLowerCase()) { // check if the artist contains your search term
+          resolve(song);      
+        }
+    });
+  });
+});
+}
+
+function searchForSong(songName, artistName="", fetchLyrics=false) { // changed artistName to have a default of "", so we can do searchForSong(songName);
+ 
+  querySong(songName, artistName).then((song) => {
+    if(song.id == 0) {
+      console.log("Invalid Song ID.");
+    }
+ 
+    console.log("Song Name: " + song.title);
+    console.log("Song ID: " + song.id);
+    console.log("Song Artist:" + song.primary_artist.name);
+   
+    if(fetchLyrics) {
+      lyricist.song(song.id, { fetchLyrics: true }).then((results) => {
+        console.log(results.lyrics);
+      });
+    }
+  });
+}
+
